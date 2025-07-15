69a7ee499b3b09088693cf7c72c7ecc7841c4a2d
Akari <akkarinn@qq.com>
2025-07-15 13:49:08 +0800

    Publish non-mc module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
@@ -6,6 +8,13 @@ plugins {
     alias(libs.plugins.loom) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.shadow) apply false
+    alias(libs.plugins.maven.publish) apply false
+}
+
+
+allprojects {
+    group = "io.github.iakariak"
+    version = "0.0.1"
 }
 
 subprojects {
@@ -25,6 +34,62 @@ subprojects {
                     "-Xcontext-sensitive-resolution",
                     "-Xnested-type-aliases"
                 )
+            }
+        }
+    }
+
+
+    if (project.name != "core") { // core need be published to modrinth and CF
+        apply(plugin = "com.vanniktech.maven.publish.base")
+
+        afterEvaluate {
+            plugins.withId("signing") {
+                extensions.configure<SigningExtension> {
+                    useInMemoryPgpKeys(
+                        findProperty("singing.key")?.toString(),
+                        findProperty("signing.password")?.toString()
+                    )
+                }
+            }
+        }
+
+        configure<MavenPublishBaseExtension> {
+            publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+            signAllPublications()
+
+            coordinates(
+                groupId = project.group.toString(),
+                artifactId = project.name,
+                version = project.version.toString()
+            )
+
+            pom {
+                name.set("Chinatsu")
+                description.set("A practiced Minecraft toolkit for fabric")
+                inceptionYear.set("2025")
+                url.set("https://github.com/iAkariAk/Chinatsu")
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("iakariak")
+                        name.set("Akari")
+                        url.set("https://github.com/iakariak/")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/iAkariAk/Chinatsu")
+                    connection.set("scm:git:git://github.com/iAkariAk/Chinatsu.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/iakariak/Chinatsu.git")
+                }
             }
         }
     }
