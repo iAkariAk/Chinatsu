ecd4a78cc3b838d16f064c46a057aff19e492bf9
Akari <akkarinn@qq.com>
2025-07-16 13:41:43 +0800

    Internalize utils

--- compiler/src/main/kotlin/io/github/iakariak/chinatsu/compiler/Util.kt
+++ compiler/src/main/kotlin/io/github/iakariak/chinatsu/compiler/Util.kt
@@ -11,33 +11,33 @@ import com.squareup.kotlinpoet.ksp.toClassName
 internal inline fun <reified T : Annotation> annotation() =
     T::class.qualifiedName!!
 
-inline fun Boolean.onTrue(block: () -> Unit): Boolean = also { if (it) block() }
-inline fun Boolean.onFalse(block: () -> Unit): Boolean = also { if (!it) block() }
-inline fun <R> R.onNull(block: () -> Unit): R = also { it ?: block() }
+internal inline fun Boolean.onTrue(block: () -> Unit): Boolean = also { if (it) block() }
+internal inline fun Boolean.onFalse(block: () -> Unit): Boolean = also { if (!it) block() }
+internal inline fun <R> R.onNull(block: () -> Unit): R = also { it ?: block() }
 
 
-inline fun <reified T> qualificationOf() = T::class.qualifiedName!!
-inline fun <reified T> typeNameStringOf() = T::class.qualifiedName!!
+internal inline fun <reified T> qualificationOf() = T::class.qualifiedName!!
+internal inline fun <reified T> typeNameStringOf() = T::class.qualifiedName!!
 
 
-val KSFile.fileNameWithoutExtension: String
+internal val KSFile.fileNameWithoutExtension: String
     get() = fileName.removeSuffix(".kt")
 
-val KSFile.jvmName: String
+internal val KSFile.jvmName: String
     get() = this.getAnnotationsByType(JvmName::class).firstOrNull()?.name ?: "${fileNameWithoutExtension}Kt"
 
-val KSDeclaration.absolutePath: String
+internal val KSDeclaration.absolutePath: String
     get() = packageName.asString() + "." + simpleName.asString()
 
 
-fun KSPropertyDeclaration.toMemberName(): MemberName {
+internal fun KSPropertyDeclaration.toMemberName(): MemberName {
     val parent = parent as? KSClassDeclaration
     val isTopLevel = parent == null
     return if (isTopLevel) MemberName(packageName.asString(), simpleName.asString())
     else MemberName(parent.toClassName(), simpleName.asString())
 }
 
-fun KSFunctionDeclaration.toMemberName(): MemberName {
+internal fun KSFunctionDeclaration.toMemberName(): MemberName {
     val parent = parent as? KSClassDeclaration
     val isTopLevel = parent == null
     return if (isTopLevel) MemberName(packageName.asString(), simpleName.asString())
